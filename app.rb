da298e5d595019a140a1ed4f12129284d5e5b060
Rogier Wessel <rhwessel@xs4all.nl>
2017-12-16 10:53:43 +0100

    Update app

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'csv'
 require 'dotenv'
 require 'json'
+require 'loofah'
 require 'sentimental'
 require 'yt'
 
@@ -33,37 +35,51 @@ end
 
 def fetch_youtube_stats(video_id)
   video = Yt::Video.new id: video_id
+  v = {}
 
   # video description
-  puts "video.title: #{video.title}"
-  puts "video.published_at: #{video.published_at}"
-  puts "video.description: #{video.description}"
-  puts "video.tags: #{video.tags}"
-  puts "video.channel_id: #{video.channel_id}"
-  puts "video.channel_title: #{video.channel_title}"
-  puts "video.category_id: #{video.category_id}"
-  puts "video.category_title: #{video.category_title}"
+  v['video_id'] = video_id
+  v['video.title'] = Loofah.fragment(video.title).text
+  v['video.published_at'] = video.published_at
+  v['video.tags'] = video.tags
+  v['video.channel_id'] = video.channel_id
+  v['video.category_id'] = video.category_id
+  v['video.category_title'] = video.category_title
+  v['video.view_count'] = video.view_count
+  v['video.like_count'] = video.like_count
+  v['video.dislike_count'] = video.dislike_count
+  v['video.favorite_count'] = video.favorite_count
+  v['video.comment_count'] = video.comment_count
 
-  # video statistics
-  puts "video.view_count: #{video.view_count}"
-  puts "video.like_count: #{video.like_count}"
-  puts "video.dislike_count: #{video.dislike_count}"
-  puts "video.favorite_count: #{video.favorite_count}"
-  puts "video.comment_count: #{video.comment_count}"
-
+  # video comments
+  c = []
   video.comment_threads.each do |comment_thread|
-    sentiment = @analyzer.sentiment comment_thread.text_display
-    score = @analyzer.score comment_thread.text_display
-    puts "#{sentiment}|#{score}|#{comment_thread.text_display}"
+    # clean the comment from html mayhem
+    comment = Loofah.fragment(comment_thread.text_display).text
+    score = @analyzer.score comment
+    c << score
   end
+  len = c.length
+  if len > 2
+    v['lowest'] = c.min
+    v['highest'] = c.max
+    total = c.inject(:+)
+    v['average'] = total.to_f / len
+    sorted = c.sort
+    v['median'] = len.odd? ? sorted[len / 2] : (sorted[len / 2 - 1] + sorted[len / 2]).to_f / 2
+  end
+  puts v.to_s
+  v
 end
 
 initialize
-
-Dir['input/*'].each do |filename|
-  videos = fetch_videos(filename)
-  puts "#{videos['title']}#{videos['videos']}"
-  videos['videos'].each_value do |v|
-    fetch_youtube_stats v
+CSV.open('data.csv', 'a+', {:col_sep => "####"}) do |csv|
+  Dir['input/*'].each do |filename|
+    videos = fetch_videos(filename)
+    puts "#{videos['title']}#{videos['videos']}"
+    videos['videos'].each_value do |v|
+      t = fetch_youtube_stats v
+      csv << t.values
+    end
   end
 end
